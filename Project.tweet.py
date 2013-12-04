c6302785d2cd764a8d811636e8e56da6cafde20e
Barrob16 <RBartels02@gmail.com>
2013-12-03 17:28:50 -0800

    Create Project.tweet.py

---
+++ Project.tweet.py
@@ -0,0 +1,16 @@
+file1 = open('/Users/Barrob16/Documents/Project/htweet.txt')
+file2 = open('/Users/Barrob16/Documents/Project/hhashtag.txt')
+
+import markovgen
+import hashtaggen
+import time
+
+markov = markovgen.Markov(file1)
+markov2 = hashtaggen.Hashtag(file2)
+
+for i in range(1):
+	print ''
+	print markov.generate_markov_text(), '#', markov2.generate_markov_text() 
+	print ''
+
+ 
